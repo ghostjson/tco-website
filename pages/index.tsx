9000a695a7d2bcc751b0047ae59504dc8b6d5e65
Jesswin W Varghese <jesswinw@gmail.com>
2021-06-09 16:20:59 +0530

    resolved an issue with brand logo size

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <img
               src='/images/main-logo.png'
               alt='main-logo'
-              className='h-10 md:h-14'
+              className='h-8 md:h-14'
             />
             <svg
               height='15'
