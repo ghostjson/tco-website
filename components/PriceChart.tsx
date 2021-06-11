99324446bbfa5d6114ed5966164cbf092796b390
Jesswin W Varghese <jesswinw@gmail.com>
2021-06-11 13:53:16 +0530

    feat: responsive view

--- components/PriceChart.tsx
+++ components/PriceChart.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type PriceChartType = {
   price: string;
   title: string;
@@ -7,36 +9,43 @@ type PriceChartType = {
 };
 
 const PriceChart = ({ price, title, desc, steps, color }: PriceChartType) => {
+  const [showPricing, setShowPricing] = useState<boolean>(false);
   return (
     <div className='flex flex-col items-start w-3/4'>
-      <h2 className='text-5xl font-semibold mb-2' style={{ color: color }}>
+      <h2
+        className='text-3xl xl:text-5xl font-semibold mb-2'
+        style={{ color: color }}
+      >
         ${price}
       </h2>
-      <h4 className='font-bold text-xl uppercase tracking-wider mb-0'>
+      <h4 className='font-bold text-base xl:text-xl uppercase tracking-wider mb-0'>
         {title}
       </h4>
-      <p className='w-3/4'>{desc}</p>
+      <p className='xl:w-3/4'>{desc}</p>
       <button
-        className='text-white font-bold p-3 uppercase self-start rounded-sm my-4'
+        className='text-sm xl:text-base text-white font-bold p-1.5 xl:p-3 uppercase self-start rounded-sm my-4'
         style={{ backgroundColor: color }}
+        onClick={() => setShowPricing(!showPricing)}
       >
         How it Works
       </button>
-      <div className='flex flex-col items-start space-y-6 mt-8 w-2/4'>
-        {steps.map((step, index) => {
-          return (
-            <span
-              key={index}
-              className='relative px-2'
-              style={{ color: color }}
-            >
-              <h6 className='font-bold'>STEP {index + 1}</h6>
-              {step}
-              <div className='absolute rounded-full h-1 w-1 bg-current top-2.5 left-0'></div>
-            </span>
-          );
-        })}
-      </div>
+      {showPricing && (
+        <div className='flex flex-col items-start space-y-6 mt-8 xl:w-2/4'>
+          {steps.map((step, index) => {
+            return (
+              <span
+                key={index}
+                className='relative px-2'
+                style={{ color: color }}
+              >
+                <h6 className='font-bold'>STEP {index + 1}</h6>
+                {step}
+                <div className='absolute rounded-full h-1 w-1 bg-current top-2.5 left-0'></div>
+              </span>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- pages/services/marketing.tsx
+++ pages/services/marketing.tsx
@@ -6,7 +6,7 @@ const Marketing = () => {
   return (
     <Layout>
       <div
-        className='h-[40vh] xl:h-[50vh] relative flow-root '
+        className='h-[30vh] md:h-[40vh] xl:h-[50vh] relative flow-root '
         style={{
           background:
             'linear-gradient(223.76deg, #C96226 6.81%, #CD8334 89.5%)',
@@ -15,7 +15,7 @@ const Marketing = () => {
         <img
           src='/images/main-logo.png'
           alt='main-logo'
-          className='mt-14 mx-auto md:mt-16 h-8 md:h-10  md:ml-20'
+          className='mt-14 mx-auto md:mt-16 h-6 md:h-10  md:ml-20'
         />
         <img
           src='/illustrations/marketing-banner.svg'
@@ -23,18 +23,18 @@ const Marketing = () => {
           className='absolute -bottom-20 md:-bottom-1/4 xl:bottom-auto xl:top-[20%]'
         />
       </div>
-      <div className='mt-[45vh] flex flex-col items-center p-16 px-16'>
+      <div className='mt-[20vh] xl:mt-[45vh] flex flex-col items-center p-4 xl:p-16'>
         <span className='relative py-2'>
-          <h2 className='text-4xl uppercase font-semibold text-[#3A7A98]'>
+          <h2 className='text-4xl uppercase font-semibold text-[#3A7A98] text-center'>
             Marketing Design
           </h2>
           <div className='absolute h-0.5 w-1/4 bottom-0 bg-[#3A7A98] left-1/2 transform -translate-x-1/2 rounded-full'></div>
         </span>
-        <p className='text-xl text-center mt-2 text-[#3A7A98]'>
+        <p className='text-lg xl:text-xl text-center mt-2 text-[#3A7A98]'>
           We create marketing materials that communicates effectively to your
           target audiences in order to boost sales.
         </p>
-        <div className='flex w-full justify-evenly items-start mt-16 px-32'>
+        <div className='flex w-full gap-3 justify-evenly items-start mt-16 px-4 xl:px-32 pb-32'>
           <PriceChart
             color='#3A7B99'
             price='50'
@@ -46,7 +46,7 @@ const Marketing = () => {
               'Select template & download. (Editable artwork in PSD/EPS/PPT or DOC format).',
             ]}
           />
-          <div className='h-96 w-1 bg-[#BCD5DA] mx-6 self-end'></div>
+          <div className='h-96 w-1 bg-[#BCD5DA] xl:mx-6 self-end hidden xl:block'></div>
           <PriceChart
             color='#27566C'
             price='350'
@@ -63,23 +63,24 @@ const Marketing = () => {
         </div>
       </div>
       <div
-        className='text-white p-16 px-8 xl:px-24 text-justify font-calibri font-regular'
+        className='text-white p-8 px-8 xl:px-24 text-justify font-calibri font-regular'
         style={{
           background: 'linear-gradient(43.38deg, #C96226 6.81%, #CD8334 89.5%)',
         }}
       >
-        <div className='flex justify-center xl:justify-between mb-0'>
+        <div className='flex justify-left xl:justify-between mb-0'>
           <h2 className='text-4xl uppercase tracking-wide'>Custom Artwork</h2>
           <SocialIconBar className='hidden xl:flex' />
         </div>
-        <div className='uppercase text-2xl -mt-2'>From $750</div>
-        <p className='text-lg md:text-xl tracking-wide mt-6 w-10/12'>
+        <div className='uppercase text-xl xl:text-2xl -mt-2'>From $750</div>
+        <p className='text-lg md:text-xl tracking-wide mt-6 xl:w-10/12'>
           A custom artwork is suitable for organizations that have specifiic
           themes or concepts to convey to their audiences. It is also ideal for
           businesses that want to sell to customers effectively like running
           online campaigns or marketing events.
         </p>
-        <p className='mt-6 text-xl'>Contact us for more information.</p>
+        <p className='mt-6 md:text-xl'>Contact us for more information.</p>
+        <SocialIconBar className='flex xl:hidden justify-end mt-6' />
       </div>
     </Layout>
   );
