e3c9eeb19ecfc7ab6a3fd3cdb1d0360105f97cf2
lemokami <jesswinw@gmail.com>
2022-01-20 10:35:03 +0530

    refactor: removed darkMode property as its not used

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       screens: {
