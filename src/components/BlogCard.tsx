2a226cccce7e215dc73b4476494ab98c49836bfc
lemokami <jesswinw@gmail.com>
2022-01-20 12:01:13 +0530

    feat: updated blog components

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,19 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 
 type BlogCardProps = {
   variant: 'dark' | 'light';
   title: string;
   image: string;
+  slug?: string;
 };
 
-const BlogCard = ({ variant, title, image }: BlogCardProps) => (
-  <article
-    className={`${
-      variant === 'light' ? ' bg-[#999999]' : 'bg-[#666666]'
-    } p-2 pl-4 xl:pl-2 w-full xl:w-1/6  flex xl:flex-col-reverse justify-between  space-x-1 text-white h-16 xl:h-auto xl:items-center`}>
-    <p className='line-clamp-2 xl:line-clamp-none xl:py-2'>{title}</p>
-    <img src={image} alt={title} className='w-12 xl:w-full self-center' />
-  </article>
+const BlogCard = ({ variant, title, image, slug }: BlogCardProps) => (
+  <Link href={slug ? `article/${slug}` : '#'}>
+    <article
+      className={`${
+        variant === 'light' ? ' bg-[#999999]' : 'bg-[#666666]'
+      } p-2 pl-4 xl:pl-2 w-full xl:w-1/6  flex xl:flex-col-reverse justify-between  space-x-1 text-white h-16 xl:h-auto xl:items-center cursor-pointer`}>
+      <p className='line-clamp-2 xl:line-clamp-none xl:py-2'>{title}</p>
+      <img src={image} alt={title} className='w-12 xl:w-full self-center' />
+    </article>
+  </Link>
 );
 
 export default BlogCard;
--- src/components/BlogPreview.tsx
+++ src/components/BlogPreview.tsx
@@ -1,21 +1,24 @@
 import BlogCard from 'src/components/BlogCard';
 
-type BlogPreviewProps = {
+interface BlogPreviewProps {
   blogData: {
+    slug: string;
     title: string;
-    image: string;
+    excerpt: string;
+    date: string;
+    cover_image: string;
   }[];
-};
-
+}
 const BlogPreview: React.FC<BlogPreviewProps> = ({ blogData }) => {
   return (
     <div className='flex flex-col xl:flex-row justify-between xl:justify-evenly items-center space-y-2 px-2 xl:px-16 my-6 xl:my-20'>
       {blogData.map((blog, index) => (
         <BlogCard
           variant={index % 2 === 0 ? 'light' : 'dark'}
-          image={blog.image}
+          image={blog.cover_image}
           key={index}
           title={blog.title}
+          slug={blog.slug}
         />
       ))}
     </div>
