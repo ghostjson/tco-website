2d88419a1790ea863ed833f58c06328fade2e83b
lemokami <jesswinw@gmail.com>
2022-01-21 10:26:33 +0530

    feat: why us footer to the bottom of each blog

--- src/pages/article/[slug].tsx
+++ src/pages/article/[slug].tsx
@@ -3,6 +3,7 @@ import path from 'path';
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
 import Layout from '@components/Layout';
+import Footer from '@components/Footer';
 
 export default function Article({ content, meta }) {
   return (
@@ -12,7 +13,13 @@ export default function Article({ content, meta }) {
         alt=''
         className='w-full h-64 object-cover brightness-75'
       />
-      <ReactMarkdown className='markdown'>{content}</ReactMarkdown>
+      {/* <div className='pb-16 pt-8 px-2 xl:px-64'> */}
+      <div className='grid grid-cols-1 md:grid-cols-6 pb-16 pt-8'>
+        <div className='col-span-0 md:col-span-1'></div>
+        <ReactMarkdown className='markdown col-span-4'>{content}</ReactMarkdown>
+        <div className='col-span-0 md:col-span-1'></div>
+      </div>
+      <Footer />
     </Layout>
   );
 }
