bdd61de2eedb2ae4e0d85b2441fd7b7e17bb9555
lemokami <jesswinw@gmail.com>
2022-01-21 10:25:24 +0530

    fix: blog preview card size issue

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -13,9 +13,13 @@ const BlogCard = ({ variant, title, image, slug }: BlogCardProps) => (
     <article
       className={`${
         variant === 'light' ? ' bg-[#999999]' : 'bg-[#666666]'
-      } p-2 pl-4 xl:pl-2 w-full xl:w-1/6  flex xl:flex-col-reverse justify-between  space-x-1 text-white h-16 xl:h-auto xl:items-center cursor-pointer`}>
-      <p className='line-clamp-2 xl:line-clamp-none xl:py-2'>{title}</p>
-      <img src={image} alt={title} className='w-12 xl:w-full self-center' />
+      } p-2 pl-4 xl:pl-2 w-full xl:w-1/6  flex xl:flex-col-reverse justify-between xl:justify-end space-x-1 text-white h-16 xl:h-auto items-center cursor-pointer`}>
+      <p className='line-clamp-2'>{title}</p>
+      <img
+        src={image}
+        alt={title}
+        className='w-16 xl:w-full xl:h-56 self-center xl:mb-2 object-cover'
+      />
     </article>
   </Link>
 );
--- src/components/BlogPreview.tsx
+++ src/components/BlogPreview.tsx
@@ -11,7 +11,7 @@ interface BlogPreviewProps {
 }
 const BlogPreview: React.FC<BlogPreviewProps> = ({ blogData }) => {
   return (
-    <div className='flex flex-col xl:flex-row justify-between xl:justify-evenly items-center space-y-2 px-2 xl:px-16 my-6 xl:my-20'>
+    <div className='flex flex-col xl:flex-row justify-between xl:justify-evenly items-stretch space-y-2 px-2 xl:px-16 my-6 xl:my-20 '>
       {blogData.map((blog, index) => (
         <BlogCard
           variant={index % 2 === 0 ? 'light' : 'dark'}
