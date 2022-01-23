32ef1d340a9cd3ccf668c35599a24f4a29a05e88
lemokami <jesswinw@gmail.com>
2022-01-23 23:33:52 +0530

    refactor:
    visual changes for quiz app

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
-import { isDesktopScreen } from "@utils/checkScreen";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import NavLink from "./NavLink";
+import { isDesktopScreen } from '@utils/checkScreen';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import NavLink from './NavLink';
 
-const Navbar = () => {
+const Navbar = ({ dark }: { dark?: boolean }) => {
   const [showNav, setShowNav] = useState(false);
   useEffect(() => {
     if (window.innerWidth < 1200) {
-      document.body.style.overflow = showNav ? "hidden" : "auto";
+      document.body.style.overflow = showNav ? 'hidden' : 'auto';
     }
   }, [showNav]);
 
@@ -15,7 +15,7 @@ const Navbar = () => {
     <>
       <div
         className={`bg-white absolute z-50 top-0 left-0 w-3/4 md:w-1/2 h-screen flex flex-col pt-32 transform transition-all xl:hidden ${
-          showNav ? "" : "-translate-x-full"
+          showNav ? '' : '-translate-x-full'
         }`}>
         {NavLinks.map((link, key) => {
           return (
@@ -31,20 +31,20 @@ const Navbar = () => {
       {/* backdrop for the navbar  */}
       <div
         className={`bg-black opacity-50 absolute w-full h-screen transform z-40 xl:bg-transparent ${
-          showNav ? "" : "-translate-x-full"
+          showNav ? '' : '-translate-x-full'
         }`}
         onClick={() => setShowNav(!showNav)}></div>
 
       {/* desktop navbar  */}
       <div
         className={`bg-white absolute top-20 left-20 z-50 rounded hidden flex-col p-2 ${
-          showNav ? "xl:flex" : ""
-        }`}>
+          dark ? 'border-2 shadow' : ''
+        } ${showNav ? 'xl:flex' : ''}`}>
         {NavLinks.map((link, key) => {
           return (
             <Link href={link.path} key={key}>
               <a
-                className="py-2 px-4 rounded transition-all hover:bg-black hover:text-white"
+                className='py-2 px-4 rounded transition-all hover:bg-black hover:text-white'
                 onClick={() => setShowNav((navState) => !navState)}>
                 {link.name}
               </a>
@@ -53,30 +53,32 @@ const Navbar = () => {
         })}
       </div>
       {/* Navbar controller  */}
-      <nav className="flex flex-col items-start absolute top-8 left-4 md:top-8 md:left-8 z-50">
+      <nav className='flex flex-col items-start absolute top-8 left-4 md:top-8 md:left-8 z-50'>
         <img
-          src="/images/tco-logo.png"
-          alt="tco-logo"
+          src='/images/tco-logo.png'
+          alt='tco-logo'
           className={`w-6 md:w-10 ${
-            showNav && !isDesktopScreen() ? "invert" : ""
+            (showNav && !isDesktopScreen()) || dark ? 'invert' : ''
           }`}
         />
-        <span className="flex gap-1 items-start justify-start">
+        <span className='flex gap-1 items-start justify-start'>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
+            xmlns='http://www.w3.org/2000/svg'
             className={`h-6 w-6 md:h-10 md:w-10  cursor-pointer ${
-              showNav && !isDesktopScreen() ? "text-black" : "text-white"
+              (showNav && !isDesktopScreen()) || dark
+                ? 'text-black'
+                : 'text-white'
             }`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
             onClick={() => setShowNav(!showNav)}
             onBlur={() => setShowNav(false)}>
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
+              strokeLinecap='round'
+              strokeLinejoin='round'
               strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
+              d='M4 6h16M4 12h16M4 18h16'
             />
           </svg>
         </span>
@@ -89,15 +91,15 @@ export default Navbar;
 
 const NavLinks = [
   {
-    name: "Home",
-    path: "/",
+    name: 'Home',
+    path: '/',
   },
   {
-    name: "Services",
-    path: "/services",
+    name: 'Services',
+    path: '/services',
   },
   {
-    name: "Resource",
-    path: "/resource",
+    name: 'Resource',
+    path: '/resource',
   },
 ];
--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+
+import Navbar from '@components/Navbar';
 import { questions, results } from 'src/constants/questions';
+import Link from 'next/link';
 
 export default function Quiz() {
   const [question, setQuestion] = useState(0);
@@ -16,35 +19,45 @@ export default function Quiz() {
   useEffect(() => setLimit(questions.length), []);
 
   return (
-    <div className='h-screen flex flex-col  justify-center px-10 xl:w-1/2 mx-auto'>
-      {question < limit ? (
-        <>
-          <h6 className='text-xl font-agaramondPro'>
-            Question {question + 1} of {limit}
-          </h6>
-          <h4 className='text-3xl mb-6 font-agaramondPro'>
-            {questions[question].question}
-          </h4>
-          <div className='flex flex-col items-stretch xl:items-start space-y-2 font-calibri px-2'>
-            {questions[question].choices.map((choice) => {
-              return (
-                <button
-                  className='p-2 px-16 rounded-md border xl:hover:bg-slate-200 xl:hover:border-slate-500 xl:hover:scale-105 transition'
-                  onClick={() => {
-                    setScore((score) => score + choice.score);
-                    setQuestion((question) => question + 1);
-                  }}>
-                  {choice.choice}
-                </button>
-              );
-            })}
-          </div>
-        </>
-      ) : (
-        <ReactMarkdown className='flex flex-col items-start space-y-4 font-calibri text-lg'>
-          {results[getResult(score)]}
-        </ReactMarkdown>
-      )}
-    </div>
+    <>
+      <Navbar dark />
+      <div className='h-screen flex flex-col  justify-center px-10 xl:w-1/2 mx-auto'>
+        {question < limit ? (
+          <>
+            <h6 className='text-xl font-agaramondPro'>
+              Question {question + 1} of {limit}
+            </h6>
+            <h4 className='text-3xl mb-6 font-agaramondPro'>
+              {questions[question].question}
+            </h4>
+            <div className='flex flex-col items-stretch xl:items-start space-y-2 font-calibri px-2'>
+              {questions[question].choices.map((choice) => {
+                return (
+                  <button
+                    className='p-2 px-16 rounded-md border xl:hover:bg-slate-200 xl:hover:border-slate-500 xl:hover:scale-105 transition'
+                    onClick={() => {
+                      setScore((score) => score + choice.score);
+                      setQuestion((question) => question + 1);
+                    }}>
+                    {choice.choice}
+                  </button>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <>
+            <ReactMarkdown className='flex flex-col items-start space-y-4 font-calibri text-lg'>
+              {results[getResult(score)]}
+            </ReactMarkdown>
+            <Link href='/'>
+              <a className='underline text-blue-700 self-center mt-10'>
+                Go back to homepage
+              </a>
+            </Link>
+          </>
+        )}
+      </div>
+    </>
   );
 }
