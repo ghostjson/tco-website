a4909ab0bd007d49d5c51ce804b1744424fc03c5
lemokami <jesswinw@gmail.com>
2022-01-28 22:50:49 +0530

    feat: welcome message in chat tab

---
+++ src/constants/quiz-message.ts
@@ -0,0 +1,13 @@
+export const welcomeMessage = `Welcome to The Creative Oracle!
+We are creatives who are passionate about design 
+& consultants who practice sound business and 
+marketing principles. 
+
+Inspired by both beauty and wisdom, we go beyond 
+creating attractive imagery to formulate strategic 
+design that works for your business. 
+
+Our company firmly believes in the design principle 
+that form follows function & that good design should 
+yield tangible results. Connect with us to learn more 
+about our team.`;
