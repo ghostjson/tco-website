677fe0122098b110dcce41bec5ce95288a41c897
lemokami <jesswinw@gmail.com>
2022-02-12 00:13:03 +0530

    feat: window scrolls to top when model is activated

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,23 @@
 import Navbar from '@components/Navbar';
 import ChatContextProvider from '@contexts/Chat';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import useStore from 'src/store';
 import Chat from './Chat';
 import RequestPortfolioModel from './RequestPortfolioModel';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { isModelOpen } = useStore((state) => state);
-
+  const ref = useRef<HTMLDivElement>();
   useEffect(() => {
-    const body = document.querySelector('body');
-    if (isModelOpen) body.style.overflow = 'hidden';
-    else body.style.overflow = 'auto';
+    const body = document.body;
+    if (isModelOpen) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      body.style.overflow = 'hidden';
+    } else body.style.overflow = 'auto';
   }, [isModelOpen]);
 
   return (
-    <div className='relative'>
+    <div className='relative' ref={ref}>
       {isModelOpen && <RequestPortfolioModel />}
       <Navbar />
       <div>{children}</div>
